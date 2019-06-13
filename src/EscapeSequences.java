b27af9c28b6a87f260241f96f9ef8e4cc2f5859f
EricGabriel7 <ethegmc@gmail.com>
2019-06-13 11:16:16 -0400

    first commit

---
+++ src/EscapeSequences.java
@@ -0,0 +1,13 @@
+public class EscapeSequences {
+    public static void main( String[] args ){
+        /*Creating the asterisk pattern with
+        \n escape sequences */
+        System.out.println("********\n" +
+                "*      *\n" +
+                "*      *\n" +
+                "*      *\n" +
+                "*      *\n" +
+                "********\n");
+    }
+
+}
